2b2728b862707cd5f7a482c0e48dc6850c6a8eda
kraimiz <stolarcukstas8@gmail.com>
2025-11-17 20:42:57 +0300

    создали дисплей, который можно закрыть

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import pygame
+
+
+pygame.init()
+
+S_W = 800
+S_H = 800
+
+running = True
+screen = pygame.display.set_mode((S_W, S_H))
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+
+
+
