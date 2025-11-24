0671022e518ef89da7dcee3b56d83c24e65f7265
kraimiz <stolarcukstas8@gmail.com>
2025-11-24 20:09:04 +0300

    ltkftv

--- bUttons.py
+++ bUttons.py
@@ -2,6 +2,8 @@ import pygame
 
 
 class Button:
-    def __init__(self):
-        self.rect = pygame.Rect(5,770,30,30)
-    def draw
+    def __init__(self, x, y, width, height,):
+        self.rect = pygame.Rect(x-width/2, y-height/2, width, height)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, 'red', self.rect)
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import pygame
+from bUttons import Button
+from config import S_W, S_H
 
 
 pygame.init()
 
-S_W = 800
-S_H = 800
-
 running = True
 screen = pygame.display.set_mode((S_W, S_H))
-
+botton = Button(S_W/2, S_H/2, 30, 30)
+botton_2 = Button(S_W/2-200, S_H/2-200, 100,50)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    botton.draw(screen, )
+    botton_2.draw(screen)
     pygame.display.update()
-
-
-
-
