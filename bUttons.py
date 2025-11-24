d8f431555d75223493254e714bedfc513ac5875b
maksonsx <maksonsx@gmail.com>
2025-11-24 20:54:48 +0300

    сделали кнопки не до конца

--- bUttons.py
+++ bUttons.py
@@ -2,8 +2,22 @@ import pygame
 
 
 class Button:
-    def __init__(self, x, y, width, height,):
+    def __init__(self, x, y, width, height,color):
         self.rect = pygame.Rect(x-width/2, y-height/2, width, height)
-
+        self.color = color
+        self.standart_color = color
+        self.active_color = 'white'
     def draw(self, screen):
-        pygame.draw.rect(screen, 'red', self.rect)
+        pygame.draw.rect(screen,self.color, self.rect,border_radius=10)
+    def update(self,screen):
+        self.change_color()
+        self.draw(screen)
+
+    def change_color(self):
+        mouse_x, mouse_y= pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_x, mouse_y):
+            self.color = self.active_color
+        else:
+            self.color =  self.standart_color
+
+
--- main.py
+++ main.py
@@ -7,12 +7,16 @@ pygame.init()
 
 running = True
 screen = pygame.display.set_mode((S_W, S_H))
-botton = Button(S_W/2, S_H/2, 30, 30)
-botton_2 = Button(S_W/2-200, S_H/2-200, 100,50)
+button_ng = Button(50, 45, 50, 20,'blue')
+button_cont = Button(S_W/2-200, S_H/2-200, 100,50,'green')
+button_save = Button(S_W/2-200, S_H/2-200, 100,50,'green')
+button_set = Button(S_W/2-200, S_H/2-200, 100,50,'green')
+button_exit = Button(S_W/2-200, S_H/2-200, 100,50,'green')
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    botton.draw(screen, )
-    botton_2.draw(screen)
+
+    button_ng.update(screen )
+    button_cont.update(screen)
     pygame.display.update()
